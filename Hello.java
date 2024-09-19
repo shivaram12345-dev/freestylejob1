8a1840a0930da5064c4dace9a00454efbe7b35a4
shivaram12345-dev <shiva.danda1988@gmail.com>
2024-09-19 11:42:09 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,14 +1,11 @@
-pipeline {
-    agent any
-    options {
-        // Timeout counter starts AFTER agent is allocated
-        timeout(time: 1, unit: 'SECONDS')
-    }
-    stages {
-        stage('Example') {
-            steps {
-                echo 'Hello World'
-            }
-        }
-    }
+public class Hello {
+
+public static void main(string[] args) {
+
+system.out.printin("Hello, World");
+
 }
+
+}
+
+// for execution  javac Hello.java & java Hello
